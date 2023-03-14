4f785c28d0409df1cbd17e827101b3f1de3becd9
dsanfealos <123487285+dsanfealos@users.noreply.github.com>
2023-03-14 13:47:28 +0100

    Creación Spring

---
+++ Prácticas/Spring/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication	//Haz tu magia, spring
+@RestController	//Contiene un controlador Rest la clase (Se prepara para recibir peticiones HTTP)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@RequestMapping("/")	//La / es cuando no tenga nada escrito
+	String hello(){
+		return "Hello World!";
+	}
+
+	@RequestMapping("/hola")	//La / es cuando no tenga nada escrito
+	String hola(){
+		return "Hola Mundo";
+	}
+
+	@RequestMapping("/mimedicacion")	//La / es cuando no tenga nada escrito
+	String mimedicacion(){
+		return "Tómate las pastillas!";
+	}
+
+
+}
